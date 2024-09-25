511e7c811c7f237a3b276e2c510fa6c42a2e8ec0
Oguzhan Furkan Andic <Oguzhan.andic@hotmail.com>
2024-09-25 13:51:31 +0200

    Update BlackJackFilen.py
    Klar med spelarens val, men behöver appliceras in i spelarens klass som en metod. ändrade lite också på listan, gjorde den till en tuple istället

--- BlackJackFilen.py
+++ BlackJackFilen.py
@@ -2,13 +2,13 @@
 import random #Detta kommer att behövas senare när vi ska börja plocka spelkort senare så att man får ett slumpmässigt kort 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-Kung: int = 13
-Drottning:int = 12
-Knäckt: int = 11
-Ess:int= '' #här behövs en placeholder för att spelaren och dealern senare ska kunna 
+# Kung: int = 13
+# Drottning:int = 12
+# Knäckt: int = 11
+# Ess:int= '' #här behövs en placeholder för att spelaren och dealern senare ska kunna 
 
 
-Kortlek: list = [1,2,3,4,5,6,7,8,9,Kung,Drottning,Knäckt,Ess]
+Kortlek: tuple = [(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9'),(11,'Knäckt'),(12,'Drottning'),(13,'Kung'),('Ess','Ess')]
 
 print(Kortlek)
 
@@ -56,15 +56,28 @@ print('...')
 #----------------------------------------------------------------------------------------------------------------------------------------------
 
 #Temporär plats för att skapa funktioner
-
+Summa: int= 0
 def Testkort()->None:
-    randomnmr=random.randint(0,12)
-    kortlek=randomnmr
-    print(f'du drog  {kortlek} vilket ger {kortlek}')
-    
+    global Summa
+    Randomkort =random.choice(Kortlek)
+    if Randomkort[1] == 'Ess':
+        while True:
+            print(f'du drog ett Ess, vilket värde ska den vara? 1 eller 14')
+            ValFörVärde: int = int(input('välj här: '))
+            if ValFörVärde== 1 or ValFörVärde== 14:
+                print(f'du valde {ValFörVärde}, vilket ger dig dig {ValFörVärde} poäng')
+                Summa += ValFörVärde
+                break
+            else:
+                print('du har inte gjort ett kortekt val, välj om!')         
+    else:
+        print(f'du drog {Randomkort[1]}, vilket ger dig {Randomkort[0]} poäng')
+        Summa += Randomkort[0]
+
 
 Testkort()
-
+Testkort()
+print('din summa är ', Summa)
 
 
 
