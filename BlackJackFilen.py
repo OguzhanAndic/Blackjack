c0da64812d00d7299c906068ef6ef2e6853b4a23
Oguzhan Furkan Andic <Oguzhan.andic@hotmail.com>
2024-09-25 20:13:58 +0200

    Update BlackJackFilen.py
    ETT FULLT FUNGERANDE PROGRAMM

--- BlackJackFilen.py
+++ BlackJackFilen.py
@@ -2,11 +2,6 @@
 import random #Detta kommer att behövas senare när vi ska börja plocka spelkort senare så att man får ett slumpmässigt kort 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# Kung: int = 13
-# Drottning:int = 12
-# Knäckt: int = 11
-# Ess:int= '' #här behövs en placeholder för att spelaren och dealern senare ska kunna 
-
 
 Kortlek: tuple = [(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9'),(11,'Knäckt'),(12,'Drottning'),(13,'Kung'),('Ess','Ess')]
 
@@ -15,20 +10,7 @@ print(Kortlek)
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-
 #Här ska klasser först skappas (en överklass för spelet, en för spelare och en för dealer)
-
-
-
-#GAMMALT
-# class Kort(): #Här skapar jag en def kring hela kortleken
-#     def __init__(self, Ess: int = 1 or 14, Kung: int= 13, Drottning: int =12, Knäckt: int = 11)-> None: #Här definneras spelets kort och vad de är värda
-#         self.Ess = Ess
-#         self.Kung = Kung
-#         self.Drottning = Drottning
-#         self.Knäckt = Knäckt
-
-
 
 class AllaSpelara: #Överklassen
     def __init__(self, poäng:int = 0, )-> None:
@@ -37,7 +19,7 @@ class AllaSpelara: #Överklassen
 
 
 class spelare(AllaSpelara): #Här gör vi spelaran med hjälp av överklassen AllaSpelare
-    def __init__(self, poäng:int =0, )-> None:
+    def __init__(self, poäng:int =0 )-> None:
         super().__init__(poäng)
 
     def Ta_kort(self) -> None:
@@ -46,19 +28,34 @@ class spelare(AllaSpelara): #Här gör vi spelaran med hjälp av överklassen Al
         if Randomkort[1] == 'Ess':
             while True:
                 print(f'du drog ett Ess, vilket värde ska den vara? 1 eller 14')
-                ValFörVärde: int = int(input('välj här: '))
+                ValFörEss:str =(input('välj här: '))
+                ValFörVärde: int =ValFörEss
                 if ValFörVärde== 1 or ValFörVärde== 14:
-                    print(f'du valde {ValFörVärde}, vilket ger dig {ValFörVärde} poäng')
-                    self.poäng += ValFörVärde
-                    break
-                else:
-                    print('du har inte gjort ett kortekt val, välj om!')         
+                    try:
+                        print(f'du valde {ValFörVärde}, vilket ger dig {ValFörVärde} poäng')
+                        self.poäng += ValFörVärde
+                        break
+                    except:
+                        print('du har inte gjort ett kortekt val, välj om!')         
         else:
             print(f'du drog {Randomkort[1]}, vilket ger dig {Randomkort[0]} poäng')
             self.poäng += Randomkort[0]
+    
     def poängkoll(self)->None: 
         print(f'din nuvarande poäng är {self.poäng}')
-
+    
+    def SpelaSpelet(self)->None:
+        while True:
+            self.poängkoll()
+            Player1Val=input('Vill du göra ett till drag? svara ja eller nej! : ').lower()
+            if Player1Val == 'ja':
+                self.Ta_kort()
+                if self.poäng > 21:
+                    print('du förlorar automatiskt, du fick mer än 21!')
+                    break
+            elif Player1Val=='nej':
+                print(f'spelaren stannar, du har totalt {self.poäng}')
+                break
 
 
 
@@ -67,11 +64,11 @@ class dealer(AllaSpelara): #Här gör vi dealer med hjälp av överklassen AllaS
         super().__init__(poäng)
     
     def Ta_kort(self) -> None:
-            print(f'dealerns nuvarande poäng är {self.poäng}')
+            print(f'Dealerns nuvarande poäng är {self.poäng}')
             Randomkort =random.choice(Kortlek)
             if Randomkort[1] == 'Ess':
                 while True:
-                    print(f'dealer drog ett Ess, dealern väljer värde mellan 1 och 14...')
+                    print(f'Dealer drog ett Ess, dealern väljer värde mellan 1 och 14...')
                     BotVal=random.randint(1,2)
                     if BotVal== 1:
                         print(f'Dealern valde {BotVal}, vilket ger Dealern {BotVal} poäng')
@@ -85,7 +82,23 @@ class dealer(AllaSpelara): #Här gör vi dealer med hjälp av överklassen AllaS
                 print(f'Dealern drog {Randomkort[1]}, vilket ger Dealern {Randomkort[0]} poäng')
                 self.poäng += Randomkort[0]
     def poängkoll(self)->None: 
-        print(f'din nuvarande poäng är {self.poäng}')
+        print(f'Dealerns nuvarande poäng är {self.poäng}')
+
+    def SpelaSpelet(self)->None:
+        while True:
+            
+            if self.poäng <17:
+                self.Ta_kort()
+                continue
+            elif self.poäng >= 17 or self.poäng == 21:
+                print('Dealern stannar!')
+                self.poängkoll()
+                break
+            elif self.poäng > 21:
+                print('Dealern har mer än 21')
+                break
+            self.poängkoll()
+            break   
 
 
 
@@ -94,7 +107,34 @@ print('...')
 #----------------------------------------------------------------------------------------------------------------------------------------------
 #Här körs spelet
 
-# Player1: spelare = spelare()
+
+
+
+
+
+
+  
+while True: 
+    Player1: spelare = spelare()
+    Player1.SpelaSpelet()
+    if Player1.poäng > 21:
+        print('Du har förlorat spelet för att du fick 21. Dealern vann!')
+        break
+    Dealer1: dealer = dealer()
+    Dealer1.SpelaSpelet()
+    if Dealer1.poäng > 21:
+        print('Dealern har förlorat spelet för att hen fick 21. Du vann!')
+        break
+    else:
+        if Player1.poäng == Dealer1.poäng:
+            print('Det blev oavgjort')
+            break
+        elif Player1.poäng > Dealer1.poäng:
+            print('Du fick högre än dealern och vinner!')
+            break
+        elif Player1.poäng < Dealer1.poäng:
+            print('Du fick mindre än dealern och förlorar!')
+            break
 
 # Player1.Ta_kort()
 # Player1.Ta_kort()
